42997f15be1032b0713b4600abaa4d664a253896
Dinesh karodiya <dineshkarodiya61@gmail.com>
2025-11-25 02:09:41 +0530

    jh

--- script.js
+++ script.js
@@ -68,3 +68,84 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+const accountModal = document.getElementById('accountModal');
+const loginModal = document.getElementById('loginModal');
+const forgotModal = document.getElementById('forgotModal');
+const createAccountBtn = document.getElementById('createAccountBtn');
+const loginModalBtn = document.getElementById('loginModalBtn');
+const goLoginBtn = document.getElementById('goLogin');
+const backBtn = document.getElementById('backBtn');
+const backLoginBtn = document.getElementById('backLogin');
+const forgotBtn = document.getElementById('forgotBtn');
+const backForgotBtn = document.getElementById('backForgot');
+const closeAccountModal = document.getElementById('closeAccountModal');
+const closeLoginModal = document.getElementById('closeLoginModal');
+const closeForgotModal = document.getElementById('closeForgotModal');
+
+createAccountBtn.onclick = () => {
+  accountModal.style.display = 'flex';
+  document.body.style.overflow = "hidden"; 
+};
+loginModalBtn.onclick = () => {
+  loginModal.style.display = 'flex';
+  document.body.style.overflow = "hidden"; 
+};
+goLoginBtn.onclick = () => {
+  accountModal.style.display = 'none';
+  loginModal.style.display = 'flex';
+};
+
+backBtn.onclick = () => {
+  accountModal.style.display = 'none';
+  document.body.style.overflow = "auto";
+};
+backLoginBtn.onclick = () => {
+  loginModal.style.display = 'none';
+  document.body.style.overflow = "auto"; 
+};
+forgotBtn.onclick = () => {
+  loginModal.style.display = 'none';
+  forgotModal.style.display = 'flex';
+};
+backForgotBtn.onclick = () => {
+  forgotModal.style.display = 'none';
+  loginModal.style.display = 'flex';
+};
+closeAccountModal.onclick = () => {
+  accountModal.style.display = 'none';
+  document.body.style.overflow = "auto";
+};
+closeLoginModal.onclick = () => {
+  loginModal.style.display = 'none';
+  document.body.style.overflow = "auto";
+};
+closeForgotModal.onclick = () => {
+  forgotModal.style.display = 'none';
+  document.body.style.overflow = "auto";
+};
+
+window.onclick = (e) => {
+  if (e.target === accountModal) {
+    accountModal.style.display = 'none';
+    document.body.style.overflow = "auto";  
+  }
+  if (e.target === loginModal) {
+    loginModal.style.display = 'none';
+    document.body.style.overflow = "auto";   
+  }
+  if (e.target === forgotModal) {
+    forgotModal.style.display = 'none';
+    document.body.style.overflow = "auto";  
+  }
+};
+
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('nav-links');
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+
+
